48af7bc65c154c0e5ab29601ee951946f8fc3219
NathyG2524 <nathygirma2524@gmail.com>
2022-05-31 23:56:51 +0300

    landing page integrated

--- skillz/teacher/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Lesson, course, Section
-
-# Register your models here.
-admin.site.register(course)
-admin.site.register(Section)
-admin.site.register(Lesson)
--- static_temp/README.md
+++
@@ -1,2 +0,0 @@
-<h1>Skillz Elearning</h1>
-<p>This is a Platform meant to be used by students to access online learning and taking examinations</p>
